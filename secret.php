785d48a3498537257f8672a7578c090c71336525
Jack-John-Joe <131839749+Jack-John-Joe@users.noreply.github.com>
2023-06-19 20:37:42 -0500

    Create secret.php

---
+++ secret.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<style>
+
+body {
+
+  background-color: red;
+
+}
+
+h1 {
+
+  font-family: Arial;
+
+  font-size: 24px;
+
+}
+
+h2 {
+
+  font-family: Arial;
+
+  font-size: 96px;
+
+  color: red;
+
+  text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black;
+
+}
+
+</style>
+
+</head>
+
+<body>
+
+<h1>Hi! You stumbled upon the secret page!</h1>
+
+<h2>Good Job!</h2>
+
+</body>
+
+</html>
+
